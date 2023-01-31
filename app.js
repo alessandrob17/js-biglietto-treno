d4e3fdca0ec0f467ec7b995b21badf08cf8865b2
alessandrob17 <a.baldipardi@gmail.com>
2023-01-31 16:17:30 +0100

    FEAT prink km

---
+++ app.js
@@ -0,0 +1,15 @@
+let chilometri = prompt('Quanti chilometri vuoi percorrere?')
+
+let anni = prompt('Quanti anni hai?')
+
+
+const answer = chilometri + anni
+console.log(answer)
+
+//PREZZO BIGLIETTO
+
+console.log(chilometri * 0.21)
+
+if (anni < 18){
+    console.log(chilometri * 0.21 ) / 100
+}
